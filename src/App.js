e9d837602c8480fe7e0d8b649d880117923243c9
Anupa <anupa@gmail.com>
2025-01-08 03:12:38 +0530

    initialize React application with basic structure and configuration

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '24px' }}>
+      Hello, This is my first Azure DevOps pipeline.    </div>
+  );
+}
+
+export default App;
